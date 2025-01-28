635d5c925b555d6188c7683ed62c6aaf54a4119b
plasticono <friedboy@comcast.net>
2025-01-28 12:14:40 -0500

    Added a button to activity_main.xml and allowed the user to click it and change the text of the button on click

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,27 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //can all be done on one line by taking the object
+        //from #findViewById and accessing the onClickListener directly from it.
+        button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(){
+            button.text = "clicked"
+        }
+
     }
 }
